d8a4f8f4d869b8a00402f1c9eb14146daebff5a6
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-09 10:41:45 +0600

    Add files via upload

---
+++ 1985A - Creating Words.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+  
+    
+    for(int i=0;i<t;i++){
+        string a,b;
+        cin>>a>>b;
+        char temp=a[0];
+        a[0]=b[0];
+        b[0]=temp;
+        
+    
+    cout<<a<<" "<<b<<endl;
+    }
+   
+   
+
+    return 0;
+}
