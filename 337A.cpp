9031524cea3370bb13655dea83f76889fd6fe1fe
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-06 21:00:42 +0600

    Add files via upload

--- 337A.cpp
+++ 337A.cpp
@@ -1,23 +1,45 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <set>
+#include <vector>
 using namespace std;
 
+// Function to calculate the sum of first n natural numbers
+int sumOfFirstN(int n) {
+    return (n * (n + 1)) / 2;
+}
+
+void solve() {
+    int N, C;
+    cin >> N >> C;
+    string S;
+    cin >> S;
+    
+    set<char> unique_languages(S.begin(), S.end());
+    int num_languages = unique_languages.size();
+    
+    // Option 1: Use translators only
+    int translator_cost = sumOfFirstN(num_languages);
+    
+    // Option 2: Learn one language and use translators for the rest
+    int min_cost = translator_cost;
+    for (char lang : unique_languages) {
+        int remaining_languages = num_languages - 1;
+        int cost = C + sumOfFirstN(remaining_languages);
+        min_cost = min(min_cost, cost);
+    }
+    
+    cout << min_cost << "\n";
+}
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	int f[m];
-	for (int i = 0; i < m; i++) {
-		cin >> f[i];
-	}
-	sort(f, f + m);
-	int ans = INT_MAX;
-	// 5 7 10 10 12 22
-	// 0 1  2  3  4  5
-	for (int i = 0; i < (m - n) + 1; i++) {
-		ans = min(ans, f[i + n - 1] - f[i]);
-	}
-	cout << ans << '\n';
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    
+    return 0;
 }
