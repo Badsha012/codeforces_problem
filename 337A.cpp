9ab958775948dc08abd1c3c02190c16b54c497d9
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-07 11:23:06 +0600

    Add files via upload

--- 337A.cpp
+++ 337A.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
-#include <set>
 #include <vector>
-using namespace std;
-
-// Function to calculate the sum of first n natural numbers
-int sumOfFirstN(int n) {
-    return (n * (n + 1)) / 2;
-}
-
-void solve() {
-    int N, C;
-    cin >> N >> C;
-    string S;
-    cin >> S;
-    
-    set<char> unique_languages(S.begin(), S.end());
-    int num_languages = unique_languages.size();
-    
-    // Option 1: Use translators only
-    int translator_cost = sumOfFirstN(num_languages);
-    
-    // Option 2: Learn one language and use translators for the rest
-    int min_cost = translator_cost;
-    for (char lang : unique_languages) {
-        int remaining_languages = num_languages - 1;
-        int cost = C + sumOfFirstN(remaining_languages);
-        min_cost = min(min_cost, cost);
-    }
-    
-    cout << min_cost << "\n";
-}
+#include <algorithm>
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> puzzles(m);
     
-    int T;
-    cin >> T;
-    while (T--) {
-        solve();
+    for (int i = 0; i < m; ++i) {
+        std::cin >> puzzles[i];
     }
+    
+    // Sort the puzzles
+    std::sort(puzzles.begin(), puzzles.end());
+    
+    int min_difference = INT_MAX;
+    
+    // Check each window of size n
+    for (int i = 0; i <= m - n; ++i) {
+        int current_difference = puzzles[i + n - 1] - puzzles[i];
+        min_difference = std::min(min_difference, current_difference);
+    }
+    
+    std::cout << min_difference << std::endl;
     
     return 0;
 }
