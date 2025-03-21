f84a173fbed43c31a2ad40fc3f424a24eeafe43e
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-21 19:51:36 +0600

    Add files via upload

---
+++ 1358A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int l=(a*b+1)/2;
+        cout<<l<<endl;
+    }
+}
