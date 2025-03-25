6e0edc20881eab04a40baa8cf3f21fee21caa434
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-25 09:06:54 +0600

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+#while loop
+#while loop is used to iterate over a block of code as long as the test expression is true.
+#Syntax:
+#while test_expression:
+#    Body of while
+#The body of the loop is entered only if the test_expression evaluates to True. After one iteration, the test expression is checked again. This process continues until the test_expression evaluates to False.
+x=1
+while x<=5:
+    print(x)
+    x+=1
+    
